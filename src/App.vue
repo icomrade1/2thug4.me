54de659416f18efee41c7b8c1ae0351e9ae40ff5
icomrade1 <denizhan_2006_34@hotmail.com>
2022-07-06 01:44:35 +0300

    :nerd:

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script>
-document.getElementById("text").innerHTML = ip;
 </script>
 
 <template>
-<body>
- <div id="text"></div>
+<body id="body" onclick="">
+
+<audio id="player" autoplay ><source src="./assets/music/music.mp3" type="audio/mp3"></audio>
+<a href="../thug.html"> sex </a>
+ <span id="text">hi.</span>
  </body>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -6,11 +6,25 @@ createApp(App).mount('#app')
 function json(url) {
     return fetch(url).then(res => res.json());
   }
-  
+  document.addEventListener('click', musicPlay);
+function musicPlay() {
+    document.getElementById('player').play();
+    document.removeEventListener('click', musicPlay);
+}
+
   let apiKey = '3bbd39cf30ae65e94f6625d5d86edb96727d6f25ac49c4f977df3ddd';
   json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
-    console.log(data.ip);
-    console.log(data.city);
-    console.log(data.country_code);
+    let ip = (data.ip);
+    let city = (data.city);
+    let region = (data.region);
+    let dude = (data.latitude);
+    let brt = (data.longitude);
+    let crt = (data.country_name);
+    let cnt = (data.continent_name);
+    document.getElementById("text").textContent = "      " +ip + "\n" + "     " + crt + ", " + cnt + "\n" + city + ", " + region + "\n" + " " + dude + ", " + brt;
+    document.getElementById("second").textContent = city;
     // so many more properties
   });
+
+
+  
