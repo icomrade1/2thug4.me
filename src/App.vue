1d5a216e5ed6f35090ecff08e67ce9723221265b
icomrade1 <denizhan_2006_34@hotmail.com>
2022-07-06 04:31:04 +0300

    german

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,11 @@
 <button class="button" onclick="musicPlay()">This website uses audio autoplay. Click anywhere to continue.</button>
  </span>
 <audio id="player" autoplay ><source src="./assets/music/music.mp3" type="audio/mp3"></audio>
-<span id="textt">you are not safe.</span>
 <test>
 
  <span id="text">disable your extensions. now.</span></test>
 
+<a id="textt">look behind you.</a>
  </body>
 </template>
 
